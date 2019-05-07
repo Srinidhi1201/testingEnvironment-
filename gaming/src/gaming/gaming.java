2d68e22c25e520a07ec80cb34268cc35e0313c70
srikanth <sri@sri-PC>
2019-05-07 16:54:36 +0530

    Launch application  and close application  is created

--- gaming/src/gaming/gaming.java
+++ gaming/src/gaming/gaming.java
@@ -1,6 +1,31 @@
 package gaming;
 
-public class gaming {
-	 int var  =5;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class gaming {
+
+	public static WebDriver  driver;
+	
+	public static String url="https://www.thegoodguys.com.au/LogonForm?myAcctMain=1&catalogId=30000&langId=-1&storeId=900";
+	@BeforeTest
+	public static void launchApp(){
+		
+		System.setProperty("webdriver.chrome.driver", "F:\\chromedriver.exe");
+		  driver = new ChromeDriver();
+		  driver.manage().deleteAllCookies();
+		  driver.manage().timeouts().implicitlyWait(2000,TimeUnit.SECONDS);
+		  driver.get(url);
+	}
+	@AfterTest
+	public static void closeApp(){
+		
+		driver.close();
+		
+	}
 }
+
